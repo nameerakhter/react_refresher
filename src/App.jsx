bb628a83b5db8a9b3b984253e1682a37f7ea8d5a
nameerakhter <akhtarnameer@gmail.com>
2024-09-10 17:31:57 +0530

    understanding how state updates with or without callback

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import { useState } from "react";
 
 const App = () => {
-  return (
-    <div>
-      App
-    </div>
-  )
-}
+  const [inc, setInc] = useState(0);
 
-export default App
+  function increaseValue() {
+    setInc((prevCounter) => prevCounter + 1);
+    setInc((prevCounter) => prevCounter + 1);
+    setInc((prevCounter) => prevCounter + 1);
+    setInc((prevCounter) => prevCounter + 1);
+  }
+
+  function decreaseValue() {
+    setInc(inc - 1);
+  }
+
+  return (
+    <div className="text-4xl flex flex-col gap-3 items-center justify-center ">
+      {inc}
+      <button
+        onClick={increaseValue}
+        className="bg-red-800 border p-4 rounded-xl"
+      >
+        increment
+      </button>
+      <button onClick={decreaseValue} className="bg-blue-700 border">
+        decrement
+      </button>
+    </div>
+  );
+};
+
+export default App;
