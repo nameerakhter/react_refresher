8b67eca9a2d61b5b9870286aa9c5d040808826f8
nameerakhter <akhtarnameer@gmail.com>
2024-12-09 09:37:58 +0530

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,12 @@
-import { useState } from "react";
 
 const App = () => {
-  const [inc, setInc] = useState(0);
-
-  function increaseValue() {
-    setInc((prevCounter) => prevCounter + 1);
-    setInc((prevCounter) => prevCounter + 1);
-    setInc((prevCounter) => prevCounter + 1);
-    setInc((prevCounter) => prevCounter + 1);
-  }
-
-  function decreaseValue() {
-    setInc(inc - 1);
-  }
 
   return (
-    <div className="text-4xl flex flex-col gap-3 items-center justify-center ">
-      {inc}
-      <button
-        onClick={increaseValue}
-        className="bg-red-800 border p-4 rounded-xl"
-      >
-        increment
-      </button>
-      <button onClick={decreaseValue} className="bg-blue-700 border">
-        decrement
-      </button>
+    <div className="text-white text-4xl ">
+      Hi there
     </div>
   );
 };
 
+console.log(App)
 export default App;
